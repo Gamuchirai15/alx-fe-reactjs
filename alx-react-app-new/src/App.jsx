e8131c92c19c185e406ecfbf466693a1961c5b87
Gamuchirai15 <gamiechif@gmail.com>
2024-11-07 16:15:15 +0200

    Add files via upload

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,7 +6,8 @@ import './App.css'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import UserProfile from './components/UserProfile'
+import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -39,6 +40,7 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile />
+      <Counter />
     </>
   )
 }
