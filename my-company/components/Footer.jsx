9004992ab53525bdb258039b14db4c68c1a0aa05
Gamuchirai15 <gamiechif@gmail.com>
2024-11-10 13:32:04 +0200

    Delete my-company/components directory

--- my-company/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-      <div style={{ padding: '10px', backgroundColor: '#333', color: 'white', textAlign: 'center' }}>
-        <p>© 2024 My Company. All Rights Reserved.</p>
-      </div>
-    );
-  }
-  
-  export default Footer;
