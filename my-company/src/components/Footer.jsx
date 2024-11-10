14f32537a7bd5f33c666ac2274b61d44320ec3eb
Gamuchirai15 <gamiechif@gmail.com>
2024-11-10 13:30:53 +0200

    Add files via upload

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <div style={{ padding: '10px', backgroundColor: '#333', color: 'white', textAlign: 'center' }}>
+        <p>© 2024 My Company. All Rights Reserved.</p>
+      </div>
+    );
+  }
+  
+  export default Footer;
