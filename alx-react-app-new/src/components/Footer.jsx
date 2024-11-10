5888c141156a340c50a64b0eb4ee43c8314f0776
Gamuchirai15 <gamiechif@gmail.com>
2024-11-10 13:55:18 +0200

    Delete alx-react-app-new/src/components directory

--- alx-react-app-new/src/components/Footer.jsx
+++
@@ -1,5 +0,0 @@
-<footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
-  <p>© 2023 City Lovers</p>
-</footer>
-
-export default Footer;
--- alx-react-app-new/src/components/MainContent.jsx
+++
@@ -1,5 +0,0 @@
-<main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
-</main>
-
-export default MainContent;
