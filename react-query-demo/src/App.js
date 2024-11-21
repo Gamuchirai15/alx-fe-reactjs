eb70066f9b90b9f45ab92f5a28d6b4c31273858a
Gamuchirai15 <gamiechif@gmail.com>
2024-11-21 10:12:30 +0200

    Add files via upload

---
+++ react-query-demo/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './PostsComponent';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';  
+
+const queryClient = new QueryClient();
+
+
+function App() {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    );
+  }
+
+export default App;
---
+++ react-query-demo/src/PostsComponent.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+
+const fetchPosts = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!res.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return res.json();
+};
+
+function PostsComponent() {
+  
+  const { data, error, isLoading, isError, refetch } = useQuery(
+    'posts', 
+    fetchPosts, 
+    {
+      staleTime: 1000 * 60 * 5, 
+      refetchOnWindowFocus: true, 
+    }
+  );
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <button onClick={() => refetch()}>Refetch Data</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
