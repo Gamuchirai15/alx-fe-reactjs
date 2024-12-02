e6b5f9a744754edac17047f9fb794a6af28d45b6
Gamuchirai15 <gamiechif@gmail.com>
2024-12-02 12:58:06 +0200

    Add files via upload

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,10 +1,17 @@
-import HomePage from "./components/HomePage";
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importing React Router components
+import HomePage from './components/HomePage'; // Importing the HomePage component
+import RecipeDetail from './components/RecipeDetail'; // Importing the RecipeDetail component
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router> {/* Wrapping the routes with Router to enable routing functionality */}
+      <Routes> {/* Defining different routes */}
+        <Route path="/" element={<HomePage />} /> {/* HomePage route */}
+        <Route path="/recipe/:id" element={<RecipeDetail />} /> {/* RecipeDetail route with dynamic id */}
+      </Routes>
+    </Router>
   );
 }
 
