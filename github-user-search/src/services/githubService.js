42c3bd502f35cbc1de06b496d83378c6d24ea7e9
Gamuchirai15 <gamiechif@gmail.com>
2024-12-13 10:36:25 +0200

    Add files via upload

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.github.com/search/users';
+const API_URL = 'https://api.github.com/search/users?q';
 
 export const fetchUserData = async (query, page = 1, location = '', minRepos = '') => {
   const perPage = 10; 
