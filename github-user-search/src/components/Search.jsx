6ae8432a4d0568d8e028186fe3fedca1a77afa2b
Gamuchirai15 <gamiechif@gmail.com>
2024-12-13 10:14:03 +0200

    Add files via upload

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -20,7 +20,7 @@ const Search = () => {
       setUserData(data);
     } catch (err) {
       // Set the error state when the user is not found or any error occurs
-      setError("Looks like we can't find the user.");
+      setError("Looks like we cant find the user.");
     } finally {
       setLoading(false);
     }
