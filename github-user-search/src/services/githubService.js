fd78895eb93989eccc8968e05aa190c5e41a2ee7
Gamuchirai15 <gamiechif@gmail.com>
2024-12-13 10:34:25 +0200

    Add files via upload

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,20 +2,28 @@ import axios from 'axios';
 
 const API_URL = 'https://api.github.com/search/users';
 
-export const fetchUserData = async (query, page = 1) => {
-  const perPage = 10;
+export const fetchUserData = async (query, page = 1, location = '', minRepos = '') => {
+  const perPage = 10; 
+  
+  let searchQuery = `type:user`;
+  
+  if (query) searchQuery += `+${query}`; 
+  if (location) searchQuery += `+location:${location}`; 
+  if (minRepos) searchQuery += `+repos:>=${minRepos}`; 
+
   try {
     const response = await axios.get(API_URL, {
       params: {
-        q: query,
+        q: searchQuery,
         page: page,
-        per_page: perPage,
+        per_page: perPage, 
       },
     });
-    return response.data; 
+    return response.data;  
   } catch (error) {
     throw new Error('Error fetching user data');
   }
 };
+
 
 
