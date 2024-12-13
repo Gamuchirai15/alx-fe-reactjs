d2afdc89d634d7e5d738dce7ef874d55cb5eac8d
Gamuchirai15 <gamiechif@gmail.com>
2024-12-13 09:43:09 +0200

    Add files via upload

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Search from './components/Search';  
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GitHub User Search</h1>
+      <Search />  {/* Placeholder for Search Component */}
+    </div>
+  );
+}
+
+export default App;
